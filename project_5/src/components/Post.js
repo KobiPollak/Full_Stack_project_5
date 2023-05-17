5e5f93ca5bba5b821bde9f5d448d78ce3ddbf15c
yohanan400 <yohananyakir@gmail.com>
2023-05-17 14:41:00 +0300

    add comments

--- project_5/src/components/Post.js
+++ project_5/src/components/Post.js
@@ -6,6 +6,7 @@ export default function Post(props) {
 
     let [selectedPostId, setSelectedPostId] = useState(1);
     let [pid, setPid] = useState(props.p.id);
+    let [commentsList, setCommentsList] = useState([]);
 
     // const handlePostClick = (postId) => {
     //     setSelectedPostId((prevSelectedPostId) => {
@@ -17,23 +18,47 @@ export default function Post(props) {
     //     });
     //   };
 
+
+    async function importComments(e){
+        const comments = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${props.p.id}`);
+        const comments_list = await comments.json();
+        setCommentsList(comments_list);
+        e.target.style.visibility = "hidden";
+    }
+
     return (
         <>
-  
-        <div
-        className={`post-card ${props.isSelected ? 'selected' : ''}`}
-        onClick={() => props.handleClick(props.p.id)}
-        >
-        <div className="post-card-title">
-            <label>Title: </label>
-            <label>{props.p.title}</label>
-        </div>
-        <div className="post-card-body">
-            <label>Body: </label>
-            <label>{props.p.body}</label>
-        </div>
-        </div>
 
+            <div
+                className={`post-card ${props.isSelected ? 'selected' : ''}`}
+                onClick={() => props.handleClick(props.p.id)}
+            >
+                <div className="post-card-title">
+                    <label>Title: </label>
+                    <label>{props.p.title}</label>
+                </div>
+                <div className="post-card-body">
+                    <label>Body: </label>
+                    <label>{props.p.body}</label>
+                </div>
+                <button style={{ visibility: props.isSelected ? "visible" : "hidden" }} onClick={(e) => importComments(e)} > Show Comments</button>
+                {commentsList.map(
+                    (comment) => <li>
+                        <div className="post-card-title">
+                            <label>Name: </label>
+                            <label>{comment.name}</label>
+                        </div>
+                        <div className="post-card-body">
+                            <label>Body: </label>
+                            <label>{comment.body}</label>
+                        </div>
+
+                    </li>
+                )}
+                <button style={{ visibility: commentsList.length === 0  ? "hidden" : "visible" }} onClick={() => setCommentsList([])} > Hide Comments</button>
+
+
+            </div>
         </>
 
     )
